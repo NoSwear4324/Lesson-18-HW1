e38872850e7e562e0f2e6caee9274294ffdb0f37
NoSwear4324 <spiridonovaroslav10@gmail.com>
2025-06-18 19:26:17 +0300

    2. Створіть тип-структуру, що містить інформацію про Працівника

---
+++ Employee.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+using namespace std;
+
+struct Employee {
+    string surname;     
+    int birthYear;       
+    string position;     
+    double salary;      
+    string education;   
+};
--- Lesson-18-HW1.cpp
+++ Lesson-18-HW1.cpp
@@ -3,12 +3,24 @@
 #include <cstdlib>
 #include <ctime>
 #include <cstdio>
+#include "Employee.h"
 using namespace std;
-
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(0, "");
 
+    Employee emp;
+    emp.surname = "Іванов";
+    emp.birthYear = 1990;
+    emp.position = "Інженер";
+    emp.salary = 15000.0;
+    emp.education = "Вища";
+
+    cout << "Прізвище: " << emp.surname << endl;
+    cout << "Рік народження: " << emp.birthYear << endl;
+    cout << "Посада: " << emp.position << endl;
+    cout << "Зарплата: " << emp.salary << endl;
+    cout << "Освіта: " << emp.education << endl;
 }
